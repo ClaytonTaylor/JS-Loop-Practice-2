eb6f0afaa304b4ca28b3dcd68853366f8208fc8f
ClaytonTaylor <claytaylor@cox.net>
2016-07-18 12:18:17 -0600

    JS-Loop-Practice-2

---
+++ JS-Loop-Practice-2.js
@@ -0,0 +1,27 @@
+var animals =['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+// #1
+
+// for(var i = 0; i < animals.length-1; i++) {
+// 	console.log(animals[i]);
+// }
+
+// #2
+
+// for(var i = 0; i < animals.length; i+=2) {
+// 	console.log(animals[i]);
+// }
+
+// #3
+
+// var animals =['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+// animals.reverse;
+// console.log(animals)
+
+// #4
+
+for(var j = 0; j < animals.length; i++) {
+for(var i=0; i<animals.length; i++) {
+ console.log(animals[i]);
+}
+}
